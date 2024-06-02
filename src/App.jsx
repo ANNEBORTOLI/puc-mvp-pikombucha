65dd9e02d6e529d77dabdc5b9d5c186a200023be
Anne Bortoli <annebortoli@gmail.com>
2024-06-02 09:49:24 -0300

    created carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Header } from "./components/Header";
+import { SliderCarousel } from "./components/SliderCarousel";
 
-import "./global.css";
 import styles from "./App.module.css";
+import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <main>
+          <SliderCarousel />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/SliderCarousel.jsx
@@ -0,0 +1,31 @@
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import banner1 from "../assets/banner-1.png";
+import banner2 from "../assets/banner-2.png";
+
+export function SliderCarousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <img src={banner1} alt="" />
+      </div>
+      <div>
+        <img src={banner2} alt="" />
+      </div>
+    </Slider>
+  );
+}
