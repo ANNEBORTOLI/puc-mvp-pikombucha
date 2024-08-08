abc2a8e1de3a6d106c6365f562c34cf230000715
Anne Bortoli <annebortoli@gmail.com>
2024-08-08 20:59:31 -0300

    Deploying my react app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/puc-mvp-pikombucha/", // Set the correct base path for GitHub Pages
+  plugins: [react()]
+});
