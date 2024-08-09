07803ae113ba763f3cbd75cdf085c1b304034418
Anne Bortoli <annebortoli@gmail.com>
2024-08-08 21:18:02 -0300

    Deploying my react app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/puc-mvp-pikombucha/", // Set the correct base path for GitHub Pages
+  base: "/", // Set the correct base path for GitHub Pages
   plugins: [react()]
 });
